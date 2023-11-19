c0e42b9571e3b20fb1c6c7d2a79f72b3ca3a1261
milan0827 <milanrokaya2014@gmail.com>
2023-11-20 04:42:13 +0545

    Global state handling

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -1,13 +1,16 @@
+import { useAccount } from "../../Contexts/accountContexts";
 import styles from "./Account.module.css";
 
 function Account() {
+  const { balance } = useAccount();
+
   return (
     <div className={`${styles.account} body-part`}>
       <h1>
         Binod Pokharel - <span>Normal Saving</span>
       </h1>
       <p>XXXXXX</p>
-      <h2>NPR 1290</h2>
+      <h2>NPR {balance}</h2>
     </div>
   );
 }
--- src/Pages/Transactions/Transactions.jsx
+++ src/Pages/Transactions/Transactions.jsx
@@ -1,23 +1,63 @@
+import { useState } from "react";
+import { useAccount } from "../../Contexts/accountContexts";
 import styles from "./Transactions.module.css";
 
 function Transactions() {
+  const [amount, setAmount] = useState(0);
+  const [description, setDescription] = useState("");
+  const { accountDeposit } = useAccount();
+
+  function handleDeposit() {
+    accountDeposit(amount, description);
+    setAmount(0);
+    setDescription("");
+  }
+
   return (
     <div className="glassMorphism page">
-      <div className={styles.operation}>
-        <input placeholder="Enter the amount" />
-        <button>Deposit</button>
+      <div className={`${styles.operation} body-part`}>
+        <h4>Deposit money</h4>
+        <input
+          placeholder="Enter the amount"
+          value={amount}
+          onChange={(e) => setAmount(+e.target.value)}
+        />
+        <input
+          placeholder="Remarks"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className={`btn`} onClick={handleDeposit}>
+          Deposit
+        </button>
       </div>
-      <div className={styles.operation}>
+
+      <div className={`${styles.operation} body-part`}>
+        <h4>Withdraw money</h4>
         <input placeholder="Enter the amount" />
-        <button>Withdraw</button>
+        <input placeholder="Remarks" />
+        <button className={`btn`}>Withdraw</button>
       </div>
-      <div className={styles.operation}>
+      <div className={`${styles.operation} body-part`}>
+        <h4>Trasfer money</h4>
         <input placeholder="Enter the amount" />
-        <button>Request loan</button>
+        <input placeholder="Receiver account number" />
+        <input placeholder="Receiver name" />
+        <input placeholder="Remarks" />
+        <button className={`btn`}>Tranfer</button>
       </div>
-      <div className={styles.operation}>
+      <div className={`${styles.operation} body-part`}>
+        <h4>Request for loan</h4>
         <input placeholder="Enter the amount" />
-        <button>Pay loan</button>
+        <input placeholder="Purpose for loan" />
+        <input placeholder="Remarks" />
+        <button className={`btn`}>Request loan</button>
+      </div>
+      <div className={`${styles.operation} body-part`}>
+        <h4>pay the loan</h4>
+        <input placeholder="Enter the amount" />
+        <input placeholder="Remarks" />
+        <button className={`btn`}>Pay loan</button>
       </div>
     </div>
   );
