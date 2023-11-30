11db2c8d7fb95724415e6d99a52b033434f9b787
milan0827 <milanrokaya2014@gmail.com>
2023-11-30 11:47:44 +0545

    Adding useReducer and helper function

--- src/Contexts/customerContext.jsx
+++ src/Contexts/customerContext.jsx
@@ -1,4 +1,4 @@
-const { createContext } = require("react");
+const { createContext, useReducer } = require("react");
 
 const initialState = {
   nationalId: "",
@@ -10,8 +10,55 @@ const initialState = {
   }),
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "customer/create":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalId,
+      };
+    case "customer/update":
+      return { ...state, fullName: action.payload };
+    case "customer/delete":
+      return { ...state };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 const CustomerContext = createContext();
 
+const [{ nationalId, fullName, createdAt }, dispatch] = useReducer(
+  initialState,
+  reducer
+);
+
+function createCustomer(fullName, nationalId) {
+  dispatch({ type: "customer/create", payload: { fullName, nationalId } });
+}
+
+function updateCustomer(fullName) {
+  dispatch({ type: "customer/update", payload: fullName });
+}
+
+function deleteCustomer() {
+  dispatch({ type: "delete/customer" });
+}
+
 export default function CustomerProvider({ children }) {
-  return <CustomerContext.Provider>{children}</CustomerContext.Provider>;
+  return (
+    <CustomerContext.Provider
+      value={{
+        nationalId,
+        fullName,
+        createdAt,
+        createCustomer,
+        updateCustomer,
+        deleteCustomer,
+      }}
+    >
+      {children}
+    </CustomerContext.Provider>
+  );
 }
