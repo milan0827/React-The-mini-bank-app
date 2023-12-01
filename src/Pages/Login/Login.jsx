f96d58783e116f4e525a9c41bfd4eab37269a26c
milan0827 <milanrokaya2014@gmail.com>
2023-12-01 07:04:09 +0545

    Simple login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,39 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./Login.module.css";
 import { useNavigate } from "react-router-dom";
-
-const user = {
-  username: "milan0827",
-  password: "qwerty",
-};
+import { useDispatch } from "react-redux";
+import { createCustomer } from "../../store/customerSlice";
 
 function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAuth, setIsAuth] = useState(false);
+  const [nationalId, setNationalId] = useState("");
 
-  function userLogin(username, password) {
-    if (username === user.username && password === user.password)
-      setIsAuth(true);
-  }
+  const dispatch = useDispatch();
 
   function handleSumbit(e) {
     e.preventDefault();
-    if (!username || !password) return;
-
-    if (username !== user.username || password !== user.password)
-      alert("Incorrect username or password");
-
-    userLogin(username, password);
+    if (!username || !nationalId) return;
+    dispatch(createCustomer(username, nationalId));
+    navigate("/app");
   }
-
-  useEffect(
-    function () {
-      if (isAuth) navigate("/app");
-    },
-    [isAuth, navigate]
-  );
 
   return (
     <form
@@ -42,6 +25,7 @@ function Login() {
     >
       <h2>RB - The Bank App</h2>
       {/* {!isAuth && <p>Incorrect username or password</p>} */}
+
       <div className={styles.formGroup}>
         <label htmlFor="username">Username</label>
         <input
@@ -56,8 +40,8 @@ function Login() {
         <input
           type="text"
           placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={nationalId}
+          onChange={(e) => setNationalId(e.target.value)}
         />
       </div>
 
